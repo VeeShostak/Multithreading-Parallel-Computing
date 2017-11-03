57e5614f9febab8fb0f59d1916bc8e54819e59f2
VeeShostak <VeeShostak@users.noreply.github.com>
2017-11-03 10:46:15 -0400

    add variables for simulation

---
+++ StudentLibrarySimulation/src/com/github/veeshostak/Constants.java
@@ -0,0 +1,12 @@
+package com.github.veeshostak;
+
+// variables for simulation
+public class Constants {
+
+	private Constants(){
+		
+	}
+	
+	public static final int NUMBER_OF_STUDENTS = 5;
+	public static final int NUMBER_OF_BOOKS = 7;
+}
