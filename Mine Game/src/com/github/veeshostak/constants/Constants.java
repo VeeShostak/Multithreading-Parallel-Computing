deb132c69b82ff981705d1588fce03cbd157d281
VeeShostak <VeeShostak@users.noreply.github.com>
2017-11-03 13:17:27 -0400

    Variables for Mine Game

---
+++ Mine Game/src/com/github/veeshostak/constants/Constants.java
@@ -0,0 +1,18 @@
+package com.github.veeshostak.constants;
+
+// application variables
+public class Constants {
+
+	private Constants(){
+		
+	}
+	
+	public static final int NUMBER_OF_SWEEPERS = 50;
+	public static final int NUMBER_OF_LAYERS = 100;
+	public static final int BOARD_ROWS = 10;
+	public static final int BOARD_COLUMNS = 10;
+	public static final int BOARD_WIDTH = 800;
+	public static final int BOARD_HEIGHT = 650;
+	public static final String APP_NAME = "Mine Game";
+	
+}
