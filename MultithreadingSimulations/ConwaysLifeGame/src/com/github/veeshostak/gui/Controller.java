0a41154e9becb49ce2023508f6f2458d9e6bf74f
VeeShostak <VeeShostak@users.noreply.github.com>
2017-11-03 20:43:41 -0400

    Create Controller

---
+++ MultithreadingSimulations/ConwaysLifeGame/src/com/github/veeshostak/gui/Controller.java
@@ -0,0 +1,39 @@
+package com.github.veeshostak.gui;
+
+import com.github.veeshostak.constants.Constants;
+
+public class Controller implements Runnable {
+
+	private Board board;
+	
+	private volatile static boolean keepGoing = true;
+
+	public Controller(Board board) {
+		this.board = board;
+	}
+
+	@Override
+	public void run() {
+
+		while (!Thread.currentThread().isInterrupted() && keepGoing) {
+
+			board.newIteration();
+			
+			try {
+				Thread.sleep(Constants.TIME_LAG);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+				return;
+			}
+		}
+	}
+
+	public static void startThread() {
+		keepGoing = true;
+	}
+
+	public static void stopThread() {
+		Thread.currentThread().interrupt();
+		keepGoing = false;
+	}
+}
